0244d72d2e7dcc0053c8a11abb3c540ae1da4b8f
amir maavia <Maaviyamansoori222@gmail.com>
2020-06-22 18:24:16 +0500

    assignments

---
+++ app.js
@@ -0,0 +1,112 @@
+//Chapter 21-25
+// Question 1
+//var firstname = prompt("first name")
+//var lastname = prompt("last name")
+//fullname = firstname + " " + lastname
+//alert(fullname)
+//Question 1
+//Question 2
+// var favphone = prompt("what is your fav phone")
+//         var length =  favphone.length 
+//         alert("My favorite phone is: " +  " " + favphone + "length of string" + " " + length )
+//Question 2
+//Question 3
+// var country = "Pakistani"
+// alert(country.length)
+//  var n = country.indexOf("n")
+//  alert("pakistani index of 'n'" +" " + n)
+//Question 3
+//Question 4
+// var str = "Hello World"
+// var l = str.lastIndexOf("l")
+// alert(l)
+//Question 4
+//Question 5
+// var str = "pakistani"
+//       var index = 3 
+//       alert(" pakistani Character at index 3 " + str.charAt(index))
+//Question 5
+//Question 6
+// var firstname = prompt("first name")
+// var lastname = prompt("last name")
+// fullname = firstname.concat( " " + lastname)
+// alert(fullname)
+//Question 6
+//Question 7
+// var city =  "Hyderabad"
+//   var replace = city.replace("Hyder" , "islam")
+//   alert("City:"+ " " + city  +" " + "after replacement" + " " + replace)
+//Question 7
+//Question 8
+// var str = '472'
+//   var num = 472
+//   alert( str +" " + typeof(str))
+//   alert( num +" " + typeof(num))
+//Question 8
+//Question 9
+// var str = prompt("name ")
+// str1 = str.toLocaleUpperCase()
+// alert(str1)
+//Question 9
+//Question 10
+// var str = prompt("name")
+//  str1 = str.charAt(0).toUpperCase() + str.slice(1); 
+//  alert(str1)
+//Question 10
+//Question 11
+// var num = 36.36
+// num = num.toString()
+// console.log(num)
+//Question 11
+//Question 12
+// var user = prompt("Enter your name")
+
+// if((user>0 && user<=47)||(user>=58 && user<=64)||
+//        (user>=91 && user<=96)||(user>=123 && user<=127)){
+//            alert("your Name is invalid")
+//        }
+//        else{
+//            alert("your Name is correct")
+//        }
+
+//Question 12
+
+//Question 13
+// var cos = prompt("Welcome to Fruits bakery what you want sir/ma'm")
+//          var items = ["apple","mango","watermelon","strawberry","banana"]
+//          cos = cos.toLowerCase()
+//          var match = false
+//          for(i=0; i<items.length; i++){
+//          var  index =  items.indexOf(i)
+//             if (cos === items[i]){
+//                 match = true
+//                 break
+            
+            
+//             }
+//          }
+        
+//          if(match == true){
+//              alert("Yes" + " " + cos + " " + "is availeble"   )
+//          }
+//          else{
+//              alert("Sorry Sir This fruit this time finish")
+//          }
+//Question 13
+//Question 15
+// var user= prompt("Generate the password")
+//   if(user >= 65 && user <= 90  && user >= 97 && user <= 122  && user >= 48 && user <= 57){
+// alert("Your Password is valid")
+//                }
+//                else{
+//                    alert("your password is invalid")
+//                }
+
+//Question 15
+//Question 16
+// var university = "university of karachi"
+//   university = university.toUpperCase()
+//   university = university.split('')
+
+//   document.write(university)
+//Question 16
